0f34c2b9b5446d9e77c4581b8cf9c089c8cdd240
Leonato Ieiri <leonatoieiri@gmail.com>
2019-11-27 12:40:50 -0300

    Added 'open' command

--- api/models/Stage.js
+++ api/models/Stage.js
@@ -69,80 +69,130 @@ module.exports = {
 
         compiledActions = compiledActions.concat(actionResult.compiled);
       });
-    }
+    } else {
+      var nextX = currentX;
+      var nextY = currentY;
 
-    var nextX = currentX;
-    var nextY = currentY;
+      switch (actions.name) {
+        case 'if':
+          var nextAction;
+          if (tiles[currentY].line[currentX] === actions.condition.value) {
+            nextAction = actions.actions;
+          } else {
+            nextAction = actions.elseActions;
+          }
 
-    if (actions.name === 'if') {
-      var nextAction;
-      if (tiles[currentY].line[currentX] === actions.condition.value) {
-        nextAction = actions.actions;
-      } else {
-        nextAction = actions.elseActions;
-      }
+          var conditionalResult = Stage.executeAction(
+            tiles,
+            nextAction,
+            currentX,
+            currentY,
+            inventory
+          );
+          currentX = conditionalResult.x;
+          currentY = conditionalResult.y;
+          inventory = conditionalResult.inventory;
 
-      var conditionalResult = Stage.executeAction(
-        tiles,
-        nextAction,
-        currentX,
-        currentY,
-        inventory
-      );
-      currentX = conditionalResult.x;
-      currentY = conditionalResult.y;
-      inventory = conditionalResult.inventory;
+          compiledActions = compiledActions.concat(conditionalResult.compiled);
 
-      compiledActions = compiledActions.concat(conditionalResult.compiled);
-    } else if (actions.name === 'repeat') {
-      for (let index = 0; index < actions.value; index++) {
-        var iterationResult = Stage.executeAction(
-          tiles,
-          actions.actions,
-          currentX,
-          currentY,
-          inventory
-        );
-        currentX = iterationResult.x;
-        currentY = iterationResult.y;
-        inventory = iterationResult.inventory;
-
-        compiledActions = compiledActions.concat(iterationResult.compiled);
-      }
-    } else if (actions.name === 'walk') {
-      switch (actions.value) {
-        case 'right':
-          nextX++;
           break;
-        case 'left':
-          nextX--;
+        case 'repeat':
+          for (let index = 0; index < actions.value; index++) {
+            var iterationResult = Stage.executeAction(
+              tiles,
+              actions.actions,
+              currentX,
+              currentY,
+              inventory
+            );
+            currentX = iterationResult.x;
+            currentY = iterationResult.y;
+            inventory = iterationResult.inventory;
+
+            compiledActions = compiledActions.concat(iterationResult.compiled);
+          }
+
           break;
-        case 'up':
-          nextY--;
+        case 'walk':
+          switch (actions.value) {
+            case 'right':
+              nextX++;
+              break;
+            case 'left':
+              nextX--;
+              break;
+            case 'up':
+              nextY--;
+              break;
+            case 'down':
+              nextY++;
+              break;
+            default:
+              compiledActions.push('InvalidWalk');
+              break;
+          }
+          if (!tiles[nextY].line[nextX]) {
+            compiledActions.push('OutOfBounds');
+          } else if (tiles[nextY].line[nextX] === 'wall') {
+            compiledActions.push('Wall');
+          } else if (tiles[nextY].line[nextX] === 'door') {
+            compiledActions.push('Door');
+          } else {
+            compiledActions.push(`move(${actions.value})`);
+            currentX = nextX;
+            currentY = nextY;
+            if (tiles[currentY].line[currentX] === 'finish') {
+              compiledActions.push('Goal');
+            }
+          }
           break;
-        case 'down':
-          nextY++;
+
+        case 'store':
+          if (tiles[currentY].line[currentX] === 'key') {
+            inventory.push(tiles[currentY].line[currentX]);
+            compiledActions.push('GotKey');
+          }
           break;
+
+        case 'open':
+          switch (actions.value) {
+            case 'right':
+              nextX++;
+              break;
+            case 'left':
+              nextX--;
+              break;
+            case 'up':
+              nextY--;
+              break;
+            case 'down':
+              nextY++;
+              break;
+            default:
+              compiledActions.push('InvalidOpen');
+              break;
+          }
+          if (tiles[nextY].line[nextX] === 'door') {
+            var index = inventory.findIndex(element => {
+              return element === 'key';
+            });
+            if (index >= 0) {
+              compiledActions.push('OpenDoor');
+              inventory.splice(index, 1);
+              compiledActions.push(`move(${actions.value})`);
+              currentX = nextX;
+              currentY = nextY;
+            } else {
+              compiledActions.push('MissingKey');
+            }
+          } else {
+            compiledActions.push('NoDoor');
+          }
+          break;
+
         default:
-          compiledActions.push('InvalidWalk');
+          compiledActions.push('InvalidAction');
           break;
-      }
-      if (!tiles[nextY].line[nextX]) {
-        compiledActions.push('OutOfBounds');
-      } else if (tiles[nextY].line[nextX] === 'wall') {
-        compiledActions.push('Wall');
-      } else {
-        compiledActions.push(`move(${actions.value})`);
-        currentX = nextX;
-        currentY = nextY;
-        if (tiles[currentY].line[currentX] === 'finish') {
-          compiledActions.push('Goal');
-        }
-      }
-    } else if (actions.name === 'store') {
-      if (tiles[currentY].line[currentX] === 'key') {
-        inventory.push(tiles[currentY].line[currentX]);
-        compiledActions.push('GotKey');
       }
     }
 
